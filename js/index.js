68abd193e9f49abc0364720c1dcefef4dc1d4892
YasminReinhardt <yreinhardt@udesa.edu.ar>
2022-11-17 20:52:16 -0300

    termine el index html y estoy intententandi seguir con el de busqueda

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-let apiKey = `c8c96a59cf4e2e778a6bf46883490734`
+let apiKey = `c8c96a59cf4e2e778a6bf46883490734` //mi api generado con la cuenta
 let urlPeliculasPopu = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 let urlSeriesPopu = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`
-
+let urlLoMasVistoPeli= `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
 let seccion= document.querySelector(".peliculaspop")
-
+let seccion2= document.querySelector (".seriesspop")
+let seccion3= document.querySelector (".lomasvisto")
 //agarraste ka url y querias que re deuleva todos los datos en modo json 
 fetch(urlPeliculasPopu)
 
@@ -11,18 +12,17 @@ fetch(urlPeliculasPopu)
     return response.json()
 })
 
+
 .then(function (data) {
     console.log(data)
-    let finalHtml=""
+    let finalHtml="" //vacio para que se vaya rellenando a medida que pasa el for
     for (let i=0; i<5; i++) {
-        
-
-        let titulo = data.results[i].title
+        let titulo = data.results[i].title //vas agarrando dentro de la api, dependiendo de como se llame la seccion en la cual esta el titulo. Con el [i], se va modificando a medida que trascurre el id
         let fechaEstreno = data.results[i].release_date
         let imagen = data.results[i].poster_path
         let id = data.results[i].id
 
-    //ahora abria que poner que se modifique en la pagina HTML
+    //ahora habria que poner que se modifique en la pagina HTML con el inner
         finalHtml += `<a href="./detail- movie.html?idPersonaje=${id}">
         <article class="pelicula">
             <img class="imagenport" src="https://image.tmdb.org/t/p/w500/${imagen}">
@@ -52,14 +52,12 @@ fetch(urlSeriesPopu)
     console.log(data)
     let finalHtml=""
     for (let i=0; i<5; i++) {
-        
-
         let titulo = data.results[i].name
-        let fechaEstreno = data.results[i].release_date
+        let fechaEstreno = data.results[i].first_air_date
         let imagen = data.results[i].poster_path
         let id = data.results[i].id
 
-    //ahora abria que poner que se modifique en la pagina HTML
+    //ahora habria que poner que se modifique en la pagina HTML
         finalHtml += `<a href="./detail- movie.html?idPersonaje=${id}">
         <article class="pelicula">
             <img class="imagenport" src="https://image.tmdb.org/t/p/w500/${imagen}">
@@ -70,7 +68,7 @@ fetch(urlSeriesPopu)
             </a>
         </article>`
     }
-    seccion.innerHTML= finalHtml;
+    seccion2.innerHTML= finalHtml;
     return data
 })
 
@@ -78,8 +76,9 @@ fetch(urlSeriesPopu)
     console.log(error)
     return error
 })
+//TERCER FETCH
 
-fetch(urlPeliculasPopu)
+fetch(urlLoMasVistoPeli)
 
 .then(function(response){
     return response.json()
@@ -89,8 +88,6 @@ fetch(urlPeliculasPopu)
     console.log(data)
     let finalHtml=""
     for (let i=0; i<5; i++) {
-        
-
         let titulo = data.results[i].title
         let fechaEstreno = data.results[i].release_date
         let imagen = data.results[i].poster_path
@@ -107,7 +104,7 @@ fetch(urlPeliculasPopu)
             </a>
         </article>`
     }
-    seccion.innerHTML= finalHtml;
+    seccion3.innerHTML= finalHtml;
     return data
 })
 
--- js/resultadosDeBusqueda.js
+++ js/resultadosDeBusqueda.js
@@ -9,11 +9,14 @@
 
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
-const query = queryStringObj.get('busqueda');
+let query = queryStringObj.get('.busqueda');
+let infobuscada = document.querySelector('.barras');
+infobuscada.innerText = `"Resultados de busqueda para: ${infobuscada}"`;
 
 let apiKey = `c8c96a59cf4e2e778a6bf46883490734`
-const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
 let container = document.querySelector(".peliculaspop");
+
 
 fetch(url)
     .then(function (response) {
@@ -26,11 +29,12 @@ fetch(url)
 
         for (let i=0; i<info.length; i++){
             search += `<article>
-            <img src= ${info[i].image} alt= '' /> 
+            <img src="https://image.tmdb.org/t/p/w500/ ${info[i].poster_path}"/> 
             </article> `
 
         }
         container.innerHTML = search;
+        return data
 
 
 
@@ -39,14 +43,6 @@ fetch(url)
         console.log(err)
     })
 
-    // Creamos texto para el resultado de la busqueda
-    //let campoA = queryStringToObject.get('busqueda'); 
-    //let infobuscada = document.querySelector('.barras')
-    //infobuscada.innerText = `"Resultados de busqueda para: ${campoA}"`;
-        //buscador.addEventListener('input', function(){
-            //infobuscada.innerText= '';
 
-            //})
 
-    document.querySelector(".barras").innerText= `"RESULTADO DE BUSQUEDA: ${campoAEvaluar}"`;
-    console.log(campoAEvaluar);
+
