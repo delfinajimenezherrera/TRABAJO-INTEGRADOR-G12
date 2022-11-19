c3f01ed6e162a53e028b7323816b4a40802225c9
delfinajimenezherrera <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-19 18:03:10 -0300

    generos

--- js/detalles_generos.js
+++ js/detalles_generos.js
@@ -1,11 +1,11 @@
 let apiKey = `c8c96a59cf4e2e778a6bf46883490734`; //mi api generado con la cuenta
-let urlDetalleGeneroPeli = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
-let urlDetalleGeneroSerie = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+let urlDetalleGeneroPeli = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+let urlDetalleGeneroSerie = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${id}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
 
 //El listado de las películas o series pertenecientes al género que seleccionó. El end point para usar es "Discover". crear un if
 // El nombre del género. En caso de que no se encuentre el género o no exista, debe indicarse un mensaje de error. repetir lo de resultados de busqueda
 //Lista de películas o series con su foto y su nombre. Al clickear en cualquiera de las series/ películas debe redirigir al detalle (punto 4). check
-let barras= document.querySelector (".barras")
+
 //barras.innerText = `"Resultados de busqueda para: ${query}"`;
 
 let queryString = location.search //Caputramos qs
@@ -21,9 +21,7 @@ fetch (urlDetalleGeneroPeli)
     let finalHtml=""
     let info = data.results;
     console.log(info);
-    if (info.length === 0)  {
-        barras.innerText = `No existe el resultado para: ${query}`;
-    }
+  
     for (let i=0; i<info.length; i++) {
         let titulo = info[i].title //vas agarrando dentro de la api, dependiendo de como se llame la seccion en la cual esta el titulo. Con el [i], se va modificando a medida que trascurre el id
         let fechaEstreno = info[i].release_date
