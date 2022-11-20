6132d0705db74626b63fcfe0e5610a366d248121
delfinajimenezherrera <macbookpro@MacBooks-MBP.fibertel.com.ar>
2022-11-20 12:46:15 -0300

    detalle de pelicula listo

--- js/detalles_generos.js
+++ js/detalles_generos.js
@@ -1,18 +1,34 @@
 let apiKey = `c8c96a59cf4e2e778a6bf46883490734`; //mi api generado con la cuenta
 
-let urlDetalleGeneroPeli = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
-let urlDetalleGeneroSerie = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
-//El listado de las películas o series pertenecientes al género que seleccionó. El end point para usar es "Discover". crear un if
-// El nombre del género. En caso de que no se encuentre el género o no exista, debe indicarse un mensaje de error. repetir lo de resultados de busqueda
-//Lista de películas o series con su foto y su nombre. Al clickear en cualquiera de las series/ películas debe redirigir al detalle (punto 4). check
-
-//barras.innerText = `"Resultados de busqueda para: ${query}"`;
-
 let queryString = location.search //Caputramos qs
 let queryStringToObject = new URLSearchParams(queryString); //La transformamos en OL
 let id = queryStringToObject.get('idPersonaje'); 
+
+let generosPeli = "https://api.themoviedb.org/3/genre/movie/list?api_key=c8c96a59cf4e2e778a6bf46883490734&language=en-US"
+
+// titulo en barras
+fetch (generosPeli)
+.then(function(response){
+    return response.json();
+})
+.then(function(respuesta){
+    console.log(respuesta)
+let tituloGeneros = document.querySelector(".titulogen");
+console.log(tituloGeneros)
+for (let i=0; i<respuesta.genres.length; i++){
+    if(id == respuesta.genres[i].id){
+        tituloGeneros.innerText = `Peliculas de ${respuesta.genres[i].name} `
+    }
+}
+})
+
+// URL
+
+let urlDetalleGeneroPeli = `https://api.themoviedb.org/3/discover/movie?api_key=c8c96a59cf4e2e778a6bf46883490734&language=en-US&with_genres=${id}&sort_by=popularity.desc`
+
 let detGenPe = document.querySelector(".detalleGeneroPeli");
 
+// Peliculas
 fetch (urlDetalleGeneroPeli)
 .then( function(response){
     return response.json();
@@ -24,7 +40,7 @@ fetch (urlDetalleGeneroPeli)
   
     for (let i=0; i<info.length; i++) {
         let titulo = info[i].title //vas agarrando dentro de la api, dependiendo de como se llame la seccion en la cual esta el titulo. Con el [i], se va modificando a medida que trascurre el id
-        let fechaEstreno = info[i].first_air_date
+        let fechaEstreno = info[i].release_date
         let imagen = info[i].backdrop_path
         let id = info[i].id
         
@@ -47,6 +63,63 @@ fetch (urlDetalleGeneroPeli)
     return error
 })
 
-// el nombre de genero
 
+// Series
 
+let generosSeries = "https://api.themoviedb.org/3/genre/tv/list?api_key=c8c96a59cf4e2e778a6bf46883490734&language=en-US"
+
+//titulo en barras
+fetch (generosSeries)
+.then(function(response){
+    return response.json();
+})
+.then(function(respuesta){
+    console.log(respuesta)
+let tituloGeneros = document.querySelector(".titulogenseries");
+console.log(tituloGeneros)
+for (let i=0; i<respuesta.genres.length; i++){
+    if(id == respuesta.genres[i].id){
+        tituloGeneros.innerText = `Series de ${respuesta.genres[i].name} `
+    }
+}
+})
+
+// URL
+let urlDetalleGeneroSerie =`https://api.themoviedb.org/3/discover/tv?api_key=c8c96a59cf4e2e778a6bf46883490734&language=en-US&with_genres=${id}&sort_by=popularity.desc`
+
+let detGenSerie = document.querySelector(".detalleGeneroSerie");
+
+// Traemos series
+fetch (urlDetalleGeneroSerie)
+.then( function(response){
+    return response.json();
+})
+.then(function(data){
+    let finalHtml=""
+    let info = data.results;
+    console.log(info);
+  
+    for (let i=0; i<info.length; i++) {
+        let titulo = info[i].title //vas agarrando dentro de la api, dependiendo de como se llame la seccion en la cual esta el titulo. Con el [i], se va modificando a medida que trascurre el id
+        let fechaEstreno = info[i].release_date
+        let imagen = info[i].backdrop_path
+        let id = info[i].id
+        
+        finalHtml += `<a href="./detail- movie.html?idPersonaje=${id}">
+        <article class="pelicula">
+            <img class="imagenport" src="https://image.tmdb.org/t/p/w500/${imagen}">
+            <div class= "titaño">
+                <h3 class ="titulos">${titulo} </h3>
+                <h3 class="fecha"> ${fechaEstreno}</h3>
+            </div>
+            </a>
+        </article>`
+    }
+    detGenSerie.innerHTML= finalHtml;
+    return data
+})
+
+.catch(function(error){
+    console.log(error)
+    return error
+})
