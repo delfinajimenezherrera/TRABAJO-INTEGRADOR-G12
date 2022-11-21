16e3dc6e8c99fc7cd237f8b8910661b4e6c6621f
jazmin.lombardi <jlombardidibetto@udesa.edu.ar>
2022-11-21 00:41:08 -0300

    cambios

--- js/recomendacion.js
+++ js/recomendacion.js
@@ -20,48 +20,13 @@ fetch(urlRecomSerie)
 console.log (error)
 })
 
-let mostrarRec = false;
+
 let recomendarSerie = document.querySelector(".recomendarSerie")
+
 
 recomendarSerie.addEventListener("click", function(r){
     r.preventDefault();
-    if (mostrarRec) {
+    if (recomendarSerie) {
         recomendaciones.style.display="none";
         recomendaciones.innerText="Ver Recomendaciones";
     }})
-  
-
-    /* //aca es el de ej
-    let similares = document.querySelector(".peliculas")
-    let urlSimilares = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${api_key}&language=es-ES&page=1`
-
-    fetch(urlSimilares)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data);
-            
-        let results = data.results
-        
-        if(results[0]){
-            for(let i=0; i<20; i++){
-                let id_p = results[i].id
-                let info = `<article class="art-peli">
-                                <a class="peli" href="detallepeli.html?id=${id_p}&media=${media}">
-                                    <img src=${image_url + results[i].poster_path} alt=${results[i].title}>
-                                    <h3>${results[i].title}</h3>
-                                </a>
-                            </article>`
-                similares.innerHTML += info;
-            };
-        }
-        else{
-            similares.innerHTML += '<h3 class="sorry">Lo sentimos, no tenemos películas relacionadas para mostrarte.</h3>'
-        }
-        
-    })
-    .catch(function(error){
-        console.log('El error fue: ', error);
-    })
- */
