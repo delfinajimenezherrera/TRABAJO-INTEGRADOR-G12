944431e7b7f39895873332d8c5ff239d68e37307
YasminReinhardt <yreinhardt@udesa.edu.ar>
2022-11-21 12:36:10 -0300

    Sigo intentando arrgelar favoritos

--- js/favorite.js
+++ js/favorite.js
@@ -1,10 +1,10 @@
 let apiKey1 = `c8c96a59cf4e2e778a6bf46883490734`; //mi api generado con la cuenta
 //declarar una variable y ahí guardo con getitem lo que está guardado en la propiedad favoritos pelicula
 let recuperoStorageFav= localStorage.getItem ('favortios')
-let seleccionados= JSON.parse (recuperoStorageFav)
+let favortios= JSON.parse (recuperoStorageFav)
 //declarar una variable y ahí guardo con getitem lo que está guardado en la propiedad favoritos pelicula
 let recuperoStorage2= localStorage.getItem ('favortios_series')
-let seleccionados2= JSON.parse (recuperoStorage2)
+let favortios_series= JSON.parse (recuperoStorage2)
 
 
 //Capturar el contenedor de html en favoritos 
@@ -13,15 +13,15 @@ let lista2= document.querySelector(".favoritosserie")
 //EMPEZAMOS CON PELICULAS 
 // Si el local storage está vacío, quiero indicarle al usuario que no hay favoritos seleccionados 
 //usar un condicional: si seleccionados no hay seleccionados → Quiero que le diga al usuario “no hay nada en favoritos”
-if (seleccionados != null || seleccionados.lenght == 0) {
+if (favortios != null || favortios.lenght == 0) {
     lista1.innerHTML = `<p> No hay favoritos seleccionados </p>`
 
 }
     // Si hay seleccionados: pedir api los datos de todos los ids del array de personajes elegidos 
 else {
     let favortios_peli= " ";
-    for (let i=0; i <seleccionados.lenght;i++) {    
-        let url= `https://api.themoviedb.org/3/tv/${seleccionados[i]}?api_key=${apiKey1}&language=en-US`
+    for (let i=0; i <favortios.lenght;i++) {    
+        let url= `https://api.themoviedb.org/3/tv/${favortios[i]}?api_key=${apiKey1}&language=en-US`
         fecth (url)
         .then (function(response){
             return response.json()
@@ -46,15 +46,15 @@ else {
     //SEGUIIMOS CON SERIES 
     // Si el local storage está vacío, quiero indicarle al usuario que no hay favoritos seleccionados 
     //usar un condicional: si seleccionados no hay seleccionados → Quiero que le diga al usuario “no hay nada en favoritos”
-if (seleccionados2 != null || seleccionados2.lenght == 0) {
+if (favortios_series != null || favortios_series.lenght == 0) {
     lista2.innerHTML = `<p> No hay favoritos seleccionados </p>`
 
 }
     // Si hay seleccionados: pedir api los datos de todos los ids del array de personajes elegidos 
 else {
-    let favortios_series= " ";
-    for (let i=0; i <seleccionados2.lenght;i++) {    
-        let url= `https://api.themoviedb.org/3/tv/${seleccionados2[i]}?api_key=${apiKey1}&language=en-US`
+    let favoritosserie= " ";
+    for (let i=0; i <favortios_series.lenght;i++) {    
+        let url= `https://api.themoviedb.org/3/tv/${favortios_series[i]}?api_key=${apiKey1}&language=en-US`
         fecth (url)
         .then (function(response){
             return response.json()
@@ -65,12 +65,12 @@ else {
             let dia= data.release_date
             let imagen= data.poster_path 
             let id= data.id 
-            favortios_series += `<a href="./detail-serie.html?idPersonaje= ${id}">
+            favoritosserie += `<a href="./detail-serie.html?idPersonaje= ${id}">
                             <p> Titulo: ${nombre} </p>
                             <img src= "https://image.tmdb.org/t/p/w500/${imagen}" >
                             <p> Fecha: ${dia} </p>
                             </a>`
-            lista2.innerHTML=favortios_series
+            lista2.innerHTML=favoritosserie
             return data
             })
         }
